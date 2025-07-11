9dbd6c3b4cf6aca98f21feb83f3ee2658d6b7665
Meoshameka Taylor <m_taylor77@me.com>
2025-07-11 09:13:52 -0500

    initial version

---
+++ Car Rental ( JavaScript - Starter )/index.js
@@ -0,0 +1,13 @@
+function openMenu() {
+  const body = document.body;
+
+  body.classList.add("open");
+}
+
+function closeMenu() {
+  const body = document.body;
+
+  body.classList.remove("open");
+}
+
+
